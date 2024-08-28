225f8d79dbcd8f3eda20a71a023a2e5d95e54c3e
ngckinhluan <nkluantran2907@gmail.com>
2024-08-28 13:58:29 +0700

    Fix email content

--- JWT.BLL/Services/Implementation/EmailService.cs
+++ JWT.BLL/Services/Implementation/EmailService.cs
@@ -1,32 +1,42 @@
 using System.Net;
 using System.Net.Mail;
 using JWT.BLL.Services.Interface;
+using Microsoft.Extensions.Configuration;
 
 namespace JWT.BLL.Services.Implementation;
 
-public class EmailService(string smtpServer, int smtpPort, string smtpUsername, string smtpPassword)
+public class EmailService(string smtpServer, int smtpPort, string smtpUsername, string smtpPassword, IConfiguration configuration)
     : IEmailService
 {
     private readonly string _smtpServer = smtpServer;
     private readonly int _smtpPort = smtpPort;
     private readonly string _smtpUsername = smtpUsername;
     private readonly string _smtpPassword = smtpPassword;
+    private IConfiguration Configuration => configuration;
 
-    public async Task SendEmailAsync(string to, string subject, string body)
+    public async Task SendEmailAsync(string toEmail, string subject, string emailContent)
     {
-        var mailMessage = new MailMessage
+        var fromEmail = Configuration["Mail"];
+        var displayName = Configuration["DisplayName"];
+        var password = Configuration["Password"];
+        var host = Configuration["Host"];
+        var port = int.Parse(Configuration["Port"]);
+
+        using (var client = new SmtpClient(host, port))
         {
-            From = new MailAddress(_smtpUsername, "JWT Test Project"),
-            Subject = subject,
-            Body = body,
-            IsBodyHtml = true
-        };
-        mailMessage.To.Add(to);
-        using (var smtpClient = new SmtpClient(_smtpServer, _smtpPort))
-        {
-            smtpClient.Credentials = new NetworkCredential(_smtpUsername, _smtpPassword);
-            smtpClient.EnableSsl = true;
-            await smtpClient.SendMailAsync(mailMessage);
+            client.Credentials = new NetworkCredential(fromEmail, password);
+            client.EnableSsl = true;
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(fromEmail, displayName),
+                Subject = subject,
+                Body = emailContent,
+                IsBodyHtml = true,
+            };
+
+            mailMessage.To.Add(toEmail);
+            await client.SendMailAsync(mailMessage);
         }
     }
 }
--- JWT.BLL/Services/Interface/IEmailService.cs
+++ JWT.BLL/Services/Interface/IEmailService.cs
@@ -2,5 +2,5 @@ namespace JWT.BLL.Services.Interface;
 
 public interface IEmailService
 {
-    Task SendEmailAsync(string to, string subject, string body);
+    Task SendEmailAsync(string toEmail, string subject, string emailContent);
 }
